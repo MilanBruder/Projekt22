00d8c283e96c2e1c8f13def04de5b40942c738ce
Fath3r4 <milanbruder79@gmail.com>
2022-03-10 09:12:09 +0100

    SECOND RELEASE

--- Main.py
+++ Main.py
@@ -12,12 +12,22 @@ def InputFile(File):
     f= open(File, "r")
     for sor in f:
         sor=sor[0:-1].split(";")
-        példány = (str(sor[0]),str(sor[1]))
+        példány = str(sor[0]),str(sor[1])
         Lista.append(példány)
     f.close()
+
+
+
+def f2(label):
+    print(label)
+    print("1 -",Lista[0])
+    print("1 -",Lista[1])
+    x = int(input("Kérek egy számot 1-5"))
+ 
 
 
     
 
 
 f1("Teszt")
+f2("IRASKEP")
