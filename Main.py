992aeedbbbbcc5749b68992ac5e54d7df2c0de33
Fath3r4 <Milanbruder79@gmail.com>
2022-03-10 20:37:34 +0100

    THIRD

--- Main.py
+++ Main.py
@@ -20,10 +20,25 @@ def InputFile(File):
 
 def f2(label):
     print(label)
-    print("1 -",Lista[0])
-    print("1 -",Lista[1])
-    x = int(input("Kérek egy számot 1-5"))
- 
+    print("1 - ",Lista[0])
+    print("2 - ",Lista[1])
+    print("3 - ",Lista[2])
+    print("4 - ",Lista[3])
+    print("5 - ",Lista[4])
+    
+    x = int(input("Kérek egy számot 1-5: "))
+    if(x == 1):
+        print(Lista[0])
+    if(x == 2):
+        print(Lista[1])
+    if(x == 3):
+        print(Lista[2])
+
+    if(x == 4):
+        print(Lista[3])
+    if(x == 5):
+        print(Lista[4])
+        
 
 
     
