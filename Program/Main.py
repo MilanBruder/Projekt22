a8a7ff38e36045ce2a2ee5dababdf66d3afb2793
Fath3r4 <milanbruder79@gmail.com>
2022-04-07 09:11:42 +0200

    FIX

--- Program/Main.py
+++ Program/Main.py
@@ -21,6 +21,7 @@ def InputFile(File):
 
 
 def f2(label):
+    ut="T:\Bruder Milán10D\Projekt22"
     while True:
         print(label)
         print("1 - ",(listarendezes(Lista[0])))
@@ -30,33 +31,34 @@ def f2(label):
         print("5 - ",(listarendezes(Lista[4])))
         
         x = int(input("Kérek egy számot 1-5: "))
+        
         if(x == 1):
             print(listarendezes(Lista[0]))
         #Megnyitja illetve a .show argumentummal meg is mutatja azt bármely képszerkesztőben
-            O = Image.open(r"T:\Bruder Milán 10D\Projekt22\Hosoktere.jpeg")
+            O = Image.open(r"kep\Hosoktere.jpeg")
             O.show()
             webbrowser.open('https://hu.wikipedia.org/wiki/H%C5%91s%C3%B6k_tere')
             
         if(x == 2):
             print(listarendezes(Lista[1]))
-            O = Image.open(r"T:\Bruder Milán 10D\Projekt22\NotreDame.jpg")
+            O = Image.open(r"kep\NotreDame.jpg")
             O.show()
             webbrowser.open('https://hu.wikipedia.org/wiki/H%C5%91s%C3%B6k_tere')
             
         if(x == 3):
             
             print(listarendezes(Lista[2]))
-            O = Image.open(r"T:\Bruder Milán 10D\Projekt22\orszaghaz.jpg")
+            O = Image.open(r"kep\orszaghaz.jpg")
             O.show()
             webbrowser.open('https://hu.wikipedia.org/wiki/Orsz%C3%A1gh%C3%A1z')
         if(x == 4):
             print(listarendezes(Lista[3]))
-            O = Image.open(r"T:\Bruder Milán 10D\Projekt22\Casino.jpg")
+            O = Image.open(r"kep\Casino.jpg")
             O.show()
             webbrowser.open('https://www.montecarlosbm.com/en/casino-monaco')
         if(x == 5):
             print(listarendezes(Lista[4]))
-            O = Image.open(r"T:\Bruder Milán 10D\Projekt22\Hungaroring.jpg")
+            O = Image.open(r"kep\Hungaroring.jpg")
             O.show()
             webbrowser.open('https://hungaroring.hu/')
         
